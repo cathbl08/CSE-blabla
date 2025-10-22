a45e29ab701af490035fddf7da60577ba944e870
Natalia Tylek <e12332258@student.tuwien.ac.at>
2025-10-22 18:14:56 +0200

    pybind matrix

--- src/bind_bla.cpp
+++ src/bind_bla.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/pybind11.h>
 
 #include "vector.hpp"
+#include "matrix.hpp"
 
 using namespace ASC_bla;
 namespace py = pybind11;
@@ -38,6 +39,9 @@ PYBIND11_MODULE(bla, m) {
 
       .def("__rmul__", [](Vector<double> & self, double scal)
       { return Vector<double> (scal*self); })
+
+      .def("__mul__", [](Vector<double> & self, double scal)
+      { return Vector<double> (scal*self); })
       
       .def("__str__", [](const Vector<double> & self)
       {
@@ -60,6 +64,93 @@ PYBIND11_MODULE(bla, m) {
           py::bytes mem = t[1].cast<py::bytes>();
           std::memcpy(&v(0), PYBIND11_BYTES_AS_STRING(mem.ptr()), v.size()*sizeof(double));
           return v;
+        }));
+
+
+      //MATRIX 
+
+    py::class_<Matrix<double>> (m, "Matrix")
+      .def(py::init<size_t, size_t>(),
+           py::arg("rows"), py::arg("cols"), "create matrix of given size")
+
+
+      // .def("__len__", &Vector<double>::size,
+      //      "return size of vector")
+
+      .def_property_readonly("shape",
+      [](const Matrix<double, RowMajor>& self) {
+           return std::tuple(self.rows(), self.cols());
+      })
+
+
+      .def("__getitem__",
+      [](Matrix<double, RowMajor> self, std::tuple<int, int> ind) {
+           return self(std::get<0>(ind), std::get<1>(ind));
+      })
+     
+
+
+      .def("__setitem__", [](Matrix<double> & self, py::tuple i, double m) {
+        if (i.size() != 2)
+          throw py::index_error("matrix index must be a 2-tuple");
+        size_t row = i[0].cast<size_t>();
+        size_t col = i[1].cast<size_t>();
+        self(row, col) = m;
+      })
+
+      // .def("__setitem__", [](Vector<double> & self, int i, double v) {
+      //   if (i < 0) i += self.size();
+      //   if (i < 0 || i >= self.size()) throw py::index_error("vector index out of range");
+      //   self(i) = v;
+      // })
+
+
+      .def("__getitem__", [](Matrix<double> & self, int row, int col) { return self(row,col); })
+      
+
+      //TO BE DONE FOR MATRIX SLICING 
+
+      // .def("__setitem__", [](Vector<double> & self, py::slice inds, double val)
+      // {
+      //   size_t start, stop, step, n;
+      //   if (!inds.compute(self.size(), &start, &stop, &step, &n))
+      //     throw py::error_already_set();
+      //   self.range(start, stop).slice(0,step) = val;
+      // })
+      
+      .def("__add__", [](Matrix<double> & self, Matrix<double> & other)
+      { return Matrix<double> (self+other); })
+
+
+      //this doesnt work / make sense
+      .def("__rmul__", [](Matrix<double> & self, double scal)
+      { return ScaleMatExpr<double, MatrixView<double>> (scal*self); })
+      
+      .def("__mul__", [](Matrix<double> & self, double scal)
+      { return ScaleMatExpr<double, MatrixView<double>> (scal*self); })
+
+      
+      .def("__str__", [](const Matrix<double> & self)
+      {
+        std::stringstream str;
+        str << self;
+        return str.str();
+      })
+
+     .def(py::pickle(
+        [](Matrix<double> & self) { // __getstate__
+            /* return a tuple that fully encodes the state of the object */
+          return py::make_tuple(self.rows(), self.cols(),
+                                py::bytes((char*)(void*)&self(0, 0), self.rows()*self.cols()*sizeof(double)));
+        },
+        [](py::tuple t) { // __setstate__
+          if (t.size() != 2)
+            throw std::runtime_error("should be a 2-tuple!");
+
+          Matrix<double> m(t[0].cast<size_t>());
+          py::bytes mem = t[1].cast<py::bytes>();
+          std::memcpy(&m(0, 0), PYBIND11_BYTES_AS_STRING(mem.ptr()), m.rows()*m.cols()*sizeof(double));
+          return m;
         }))
     ;
 }
---
+++ src/test1.py
@@ -0,0 +1,23 @@
+from ASCsoft.bla import *
+
+x = Vector(5)
+y = Vector(5)
+
+for i in range(len(x)):
+    x[i] = i
+y[:] = 3
+
+print ("x+y =",x+y)
+
+v = Vector(3)
+v[:] = 7
+
+import pickle
+f = open("file.txt", 'wb')
+pickle.dump([2,"hello", v], f)
+del f
+
+f2 = open("file.txt", 'rb')
+val = pickle.load(f2)
+print (val)
+print (val[2])
